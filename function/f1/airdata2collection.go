cf2a1d037298db9d2ca4d507ea0d537aabe72bc6
Infospecktra <Infospecktra@gmail.com>
2021-09-24 12:19:36 -0400

    Create airdata2collection.go

---
+++ function/f1/airdata2collection.go
@@ -0,0 +1,50 @@
+package f1
+
+import (
+	"github.com/project-flogo/core/data"
+	"github.com/project-flogo/core/data/expression/function"
+)
+
+func init() {
+	function.Register(&fnAirData2Collection{})
+}
+
+type fnAirData2Collection struct {
+}
+
+func (fnAirData2Collection) Name() string {
+	return "airdata2collection"
+}
+
+func (fnAirData2Collection) Sig() (paramTypes []data.Type, isVariadic bool) {
+	return []data.Type{data.TypeObject, data.TypeArray}, false
+}
+
+func (fnAirData2Collection) Eval(params ...interface{}) (interface{}, error) {
+	airData := params[0].(map[string]interface{})
+	var dataCollection []interface{}
+	if nil != params[1] {
+		dataCollection = params[1].([]interface{})
+	} else {
+		dataCollection = make([]interface{}, 0)
+	}
+
+	log.Info("\n\n\n (fnAirData2Collection.Eval) in airData ========>", airData)
+	log.Info("(fnAirData2Collection.Eval) in dataCollection ========>", dataCollection)
+	dataCollection = append(dataCollection, map[string]interface{}{
+		"producer": "f1",
+		"name":     "gateway",
+		"value":    airData["gateway"],
+	})
+	for name, value := range airData["reading"].(map[string]interface{}) {
+		log.Info("(fnAirData2Collection.Eval) new name ========>", name)
+		dataCollection = append(dataCollection, map[string]interface{}{
+			"producer": "f1",
+			"name":     name,
+			"value":    value,
+		})
+	}
+
+	log.Info("(fnAirData2Collection.Eval) out dataCollection ========>", dataCollection)
+	return dataCollection, nil
+}
